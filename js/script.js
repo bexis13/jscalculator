a208000b4700acfa2ea06e5165d79c9b0173c241
aboseu <bexis17@gmail.com>
2016-11-13 02:29:41 +0000

    do calculator lobgic and improve the ui

---
+++ js/script.js
@@ -0,0 +1,32 @@
+var display = document.querySelector('.display');
+var numbers = document.querySelectorAll('.number');
+var answer = document.querySelector('.answer');
+var signs = document.querySelectorAll('.sign');
+var back = document.querySelector('.back');
+afterAnswer = false
+for (var i = 0; i < numbers.length; i++) {
+	numbers[i].onclick = function(){
+		if (afterAnswer) {
+			display.value = parseInt(this.innerHTML);
+			afterAnswer = false
+		}
+		else{
+			display.value += parseInt(this.innerHTML);
+		}
+	}
+}
+for (var i = 0; i < signs.length; i++) {
+	signs[i].onclick = function(){
+		display.value += this.innerHTML;
+		afterAnswer = false
+	}
+}
+
+answer.onclick = function(){
+	display.value = eval(display.value);
+	afterAnswer = true
+}
+
+back.onclick = function () {
+	display.value = "";
+}
