eacaaf0f7f64e24f1dd97a52558577d340fa0573
Abose Ukhun <bexis17@gmail.com>
2018-05-21 20:10:18 +0100

    remove uncessary spaces and add semicolons

--- js/script.js
+++ js/script.js
@@ -3,7 +3,8 @@ var numbers = document.querySelectorAll('.number');
 var answer = document.querySelector('.answer');
 var signs = document.querySelectorAll('.sign');
 var back = document.querySelector('.back');
-var afterAnswer = false
+var afterAnswer = false;
+//
 for (var i = 0; i < numbers.length; i++) {
 	numbers[i].onclick = function(){
 		if (afterAnswer) {
@@ -15,18 +16,19 @@ for (var i = 0; i < numbers.length; i++) {
 		}
 	}
 }
+//
 for (var i = 0; i < signs.length; i++) {
 	signs[i].onclick = function(){
 		display.value += this.innerHTML;
 		afterAnswer = false
 	}
 }
-
+//
 answer.onclick = function(){
 	display.value = eval(display.value);
 	afterAnswer = true
 }
-
+//
 back.onclick = function () {
 	display.value = "";
 }
